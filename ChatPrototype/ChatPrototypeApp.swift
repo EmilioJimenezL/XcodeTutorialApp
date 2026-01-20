7c26ab72660e80bfc56e1a05bf962725aa35dd19
ctochimani <122694117+ctochimani@users.noreply.github.com>
2026-01-20 16:54:16 -0600

    Initial Commit

---
+++ ChatPrototype/ChatPrototypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatPrototypeApp.swift
+//  ChatPrototype
+//
+//  Created by iOS Lab on 20/01/26.
+//
+
+import SwiftUI
+
+@main
+struct ChatPrototypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
