b54d4878c19b177d33261bb57c6ecfb7800e5ca4
EmilioJimenezL <141659948+EmilioJimenezL@users.noreply.github.com>
2026-01-27 16:51:42 -0600

    added new buttons

--- ChatPrototype/ContentView.swift
+++ ChatPrototype/ContentView.swift
@@ -7,15 +7,41 @@
 
 import SwiftUI
 
+let buttonGrid = [
+    ["delete.left.fill", "c.circle.fill", "percent", "divide"],
+    ["7","8","9","multiply"],
+    ["4","5","6","substract"],
+    ["1","2","3","add"],
+    ["invert","0","point","equal"]
+]
+
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        VStack(){
+            HStack{
+                Spacer()
+                Text("Sample Num: 123+456=579")
+                    .padding()
+            }
+            HStack{
+                Spacer()
+                Text("Sample num: 678")
+                    .padding()
+            }
+            Grid{
+                ForEach(buttonGrid, id: \.self){row in
+                    GridRow{
+                        ForEach(row, id: \.self){col in
+                            Button(action:{
+                                print("pressed" + col)
+                            }){
+                                Image(systemName: col)
+                            }
+                        }
+                    }
+                }
+            }
         }
-        .padding()
     }
 }
 
